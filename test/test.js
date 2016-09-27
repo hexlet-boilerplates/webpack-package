3b05199be51493f7377b5eb8600f216a9bb67d15
Cyril <cyril.sadovnik@gmail.com>
2016-09-27 20:37:27 +0300

    Fix typo

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ import assert from 'assert';
 import half from '../src/index.js';
 
 describe('JavascriptPackage', () => {
-  describe('#num()', () => {
+  describe('#half()', () => {
     it('should return half of given number', () => {
       assert.equal(3, half(6));
     });
