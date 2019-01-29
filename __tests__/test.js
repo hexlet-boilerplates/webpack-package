a19ca377d26d3f689e73ebd2f0328916cb96e6a9
Kirill Mokevnin <mokevnin@gmail.com>
2019-01-29 16:15:23 +0400

    fix tests

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,15 +1,10 @@
-// @flow
-
-import fs from 'fs';
+import { promises as fs } from 'fs';
 import path from 'path';
-import { promisify } from 'util';
 import init from '../src/init';
-
-const readFile = promisify(fs.readFile);
 
 beforeEach(async () => {
   const pathToHtml = path.resolve(__dirname, '__fixtures__/index.html');
-  const html = await readFile(pathToHtml, 'utf8');
+  const html = await fs.readFile(pathToHtml, 'utf8');
   document.body.innerHTML = html;
 });
 
--- src/init.js
+++ src/init.js
@@ -1,3 +1,4 @@
+import "@babel/polyfill";
 import Example from './Example';
 
 export default () => {
