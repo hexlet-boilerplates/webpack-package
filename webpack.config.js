556195900eab2266ee085d246acc53a0b9d33e9f
Serge <liho@ro.ru>
2021-07-25 12:43:07 +0300

    fix lint

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
           loader: 'babel-loader',
         },
       },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader']},
       {
         test: /\.scss$/,
         use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
@@ -28,7 +28,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'template.html'
+      template: 'template.html',
     }),
   ],
 };
