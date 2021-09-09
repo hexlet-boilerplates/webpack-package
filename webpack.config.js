4bd14002d7bcced3ed617d502a2fc99c8bebce64
Kirill Mokevnin <mokevnin@gmail.com>
2021-09-08 22:41:51 -0400

    add sass
    Signed-off-by: Kirill Mokevnin <mokevnin@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
