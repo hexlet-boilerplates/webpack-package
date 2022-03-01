84331f7b885763b62563e490ffd182f9904ce28c
Ivan Gagarinov <dzencot@gmail.com>
2022-03-01 17:48:20 +0500

    remove babel

--- __tests__/test.js
+++ __tests__/test.js
@@ -2,7 +2,11 @@
 
 import { promises as fs } from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 import init from '../src/init';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 beforeEach(async () => {
   const pathToHtml = path.resolve(__dirname, '__fixtures__/index.html');
--- webpack.config.js
+++ webpack.config.js
@@ -4,16 +4,6 @@ module.exports = {
   mode: process.env.NODE_ENV || 'development',
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
-      },
       { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
         test: /\.scss$/,
