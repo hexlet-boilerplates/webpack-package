426b83094832cee4bd42e3e6403b521f67855107
Ivan Gagarinov <dzencot@gmail.com>
2024-08-29 12:59:51 +0500

    update

--- src/init.js
+++ src/init.js
@@ -5,5 +5,6 @@ import Example from './Example.js';
 export default () => {
   const element = document.getElementById('point');
   const obj = new Example(element);
+  console.log('test');
   obj.init();
 };
