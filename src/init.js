77102f6ea3fb765d4cdb51931c1ab090c3865ba6
Ivan Gagarinov <dzencot@gmail.com>
2024-08-29 13:22:28 +0500

    Revert "update"
    This reverts commit 426b83094832cee4bd42e3e6403b521f67855107.

--- src/init.js
+++ src/init.js
@@ -5,6 +5,5 @@ import Example from './Example.js';
 export default () => {
   const element = document.getElementById('point');
   const obj = new Example(element);
-  console.log('test');
   obj.init();
 };
